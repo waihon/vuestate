40b7552244032eead4d6b53ceacf3e38573d406c
Waihon Yew <yewwaihon@gmail.com>
2019-02-14 10:35:29 +0800

    Define a state property; Vuex getters

--- src/store.js
+++ src/store.js
@@ -5,7 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'My Custom Title',
+    links: [
+      'https://google.com',
+      'https://coursetro.com',
+      'https://youtube.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
 
