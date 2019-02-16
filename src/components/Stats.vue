4985050384811f9cef87300fd0cb98034a7cf527
Waihon Yew <yewwaihon@gmail.com>
2019-02-16 09:35:14 +0800

    Add scoped style to Stats component

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -34,3 +34,29 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+  }
+</style>
